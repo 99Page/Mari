6cac3f9ef5da22eea99fed447fbfc217931966fe
노우영 <nou0ggid@gmail.com>
2025-08-19 17:53:39 +0900

    Before, After 문서

--- App/Rim/Sources/Features/Post/BlockedPostView.swift
+++ App/Rim/Sources/Features/Post/BlockedPostView.swift
@@ -12,24 +12,107 @@ import SnapKit
 import Core
 import SwiftUI
 
+/// 변경 대상
+///  
+/// # After
+///  
+/// ```swift
+///  
+/// @View
+/// class BlockedPostView: UIView {
+///   var view: some UIView {
+///     VStack("stack") {
+///       RimImageView("lockImage") {
+///         $0.image = .symbol(name: "lock.circle")
+///         $0.fgColor = .gray
+///       }
+///       .constraint { $0.size.equalTo(100) }
+///  
+///       RimLabel("message") {
+///         $0.text = "차단한 사용자의 게시물은\n볼 수 없어요."
+///         $0.textColor = .label
+///         $0.typography = .contentTitle
+///         $0.numberOfLines = 2
+///       }
+///     }
+///     .spacing(16)
+///     .alignment(.center)
+///     .constraint { $0.center.equalToSuperview() }
+///   }
+/// }
+/// ```
+/// # Before
+///  
+/// ```swift
+///class BlockedPostView: UIView {
+///    private let blockedIconView: RimImageView
+///    private let blockedMessageLabel: RimLabel
+///    private let stackView = UIStackView()
+///
+///    init() {
+///        let lockImage = RimImageView.State(image: .symbol(name: "lock.circle", fgColor: .gray))
+///        self.blockedIconView = RimImageView(state: .constant(lockImage))
+///
+///        let blockText = RimLabel.State(
+///            text: "차단한 사용자의 게시물은\n볼 수 없어요.",
+///            textColor: .label,
+///            typography: .contentTitle,
+///            numberOfLines: 2
+///        )
+///
+///        self.blockedMessageLabel = RimLabel(state: .constant(blockText))
+///        super.init(frame: .zero)
+///        setupView()
+///        makeConstraint()
+///    }
+///
+///    required init?(coder: NSCoder) {
+///        fatalError("init(coder:) has not been implemented")
+///    }
+///
+///    private func setupView() {
+///        stackView.axis = .vertical
+///        stackView.alignment = .center
+///        stackView.spacing = 16
+///
+///        addSubview(stackView)
+///
+///        stackView.addArrangedSubview(blockedIconView)
+///        stackView.addArrangedSubview(blockedMessageLabel)
+///    }
+///
+///
+///    private func makeConstraint() {
+///        stackView.snp.makeConstraints { make in
+///            make.center.equalToSuperview()
+///        }
+///
+///        blockedIconView.snp.makeConstraints { make in
+///            make.width.height.equalTo(100)
+///        }
+///    }
+///}
+/// ```
+
 class BlockedPostView: UIView {
-    private let contentView = UIView()
     private let blockedIconView: RimImageView
     private let blockedMessageLabel: RimLabel
+    private let stackView = UIStackView()
     
     init() {
         let lockImage = RimImageView.State(image: .symbol(name: "lock.circle", fgColor: .gray))
         self.blockedIconView = RimImageView(state: .constant(lockImage))
         
         let blockText = RimLabel.State(
-            text: "차단한 사용자의 게시물은\n볼 수 없어요",
-            textColor: .black,
+            text: "차단한 사용자의 게시물은\n볼 수 없어요.",
+            textColor: .label,
             typography: .contentTitle,
             numberOfLines: 2
         )
         
         self.blockedMessageLabel = RimLabel(state: .constant(blockText))
         super.init(frame: .zero)
+        setupView()
         makeConstraint()
     }
     
@@ -37,25 +120,25 @@ class BlockedPostView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setupView() {
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 16
+
+        addSubview(stackView)
+
+        stackView.addArrangedSubview(blockedIconView)
+        stackView.addArrangedSubview(blockedMessageLabel)
+    }
+    
+    
     private func makeConstraint() {
-        addSubview(contentView)
-        contentView.addSubview(blockedIconView)
-        contentView.addSubview(blockedMessageLabel)
-        
-        contentView.snp.makeConstraints { make in
-            make.centerY.centerX.equalToSuperview()
+        stackView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
         }
-        
+
         blockedIconView.snp.makeConstraints { make in
             make.width.height.equalTo(100)
-            make.top.equalToSuperview()
-            make.centerX.equalToSuperview()
-        }
-        
-        blockedMessageLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(blockedIconView.snp.bottom).offset(16)
-            make.bottom.equalToSuperview()
         }
     }
 }
